569f4864ccc026374290687fb8a8b94b52601861
Wushani Parindya <parindyagamage075@gmail.com>
2026-03-21 20:25:58 +0530

    my project upload

---
+++ demoapp/lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  String storeName;
+  String username;
+  String contact;
+  String password;
+
+  UserModel({
+    required this.storeName,
+    required this.username,
+    required this.contact,
+    required this.password,
+  });
+}
