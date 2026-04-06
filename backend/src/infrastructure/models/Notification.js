88d56552b29d9fc0aa9995539c53e716f2569f31
IT24102254 <it24102254@my.sliit.lk>
2026-04-06 19:59:58 +0530

    Add index to createdAt field in Notification model

--- backend/src/infrastructure/models/Notification.js
+++ backend/src/infrastructure/models/Notification.js
@@ -7,12 +7,14 @@ const NotificationSchema = new mongoose.Schema({
     title: { type: String, required: true },
     message: { type: String, required: true },
     isRead: { type: Boolean, default: false },
-    createdAt: { type: String },
+    createdAt: { type: String, index: true },
     updatedAt: { type: String }
 }, {
     _id: false,
     timestamps: false
 });
+
+NotificationSchema.index({ ownerId: 1, createdAt: -1 });
 
 NotificationSchema.virtual('id').get(function() {
     return this._id;
